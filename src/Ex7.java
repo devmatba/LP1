f125df966fa6b3774e3c51e4e804c92cf4b821c6
Matheus Barbosa <99769599+devmatba@users.noreply.github.com>
2024-08-22 14:01:00 -0300

    ✨ feat: Value Check
    Checks if a value is greater than 10 and displays the corresponding message.

---
+++ src/Ex7.java
@@ -0,0 +1,16 @@
+
+public class Ex7 {
+
+	public static void main(String[] args) {
+		
+		int valor = 15;
+
+        if (valor > 10) {
+            System.out.println("É MAIOR QUE 10!");
+        } else {
+            System.out.println("NÃO É MAIOR QUE 10!");
+        }
+
+	}
+
+}
