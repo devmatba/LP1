d56f1ab262667fed7f1e99e981755c0bf1c51a5c
Matheus Barbosa <99769599+devmatba@users.noreply.github.com>
2024-08-22 14:00:09 -0300

    ✨ feat: Temperature Conversion
    Converts a temperature from Fahrenheit to Celsius using the conversion formula.

---
+++ src/Ex6.java
@@ -0,0 +1,14 @@
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		
+		double temperaturaFahrenheit = 212.0;  
+       
+        double temperaturaCelsius = (temperaturaFahrenheit - 32) * 5 / 9;
+       
+        System.out.printf("A temperatura em Celsius é: %.2f°C", temperaturaCelsius);
+        
+	}
+
+}
