d362f059facf6bd21338741dc218e4aee1fc011f
Matheus Barbosa <99769599+devmatba@users.noreply.github.com>
2024-08-22 14:04:12 -0300

    ✨ feat: Ascending Order
    Orders and displays two given values in ascending order.

---
+++ src/Ex13.java
@@ -0,0 +1,17 @@
+
+public class Ex13 {
+
+	public static void main(String[] args) {
+		
+		int valor1 = 10;
+		int valor2 = 8;
+		
+		if (valor1 < valor2) {
+            System.out.println("Ordem crescente: " + valor1 + ", " + valor2);
+        } else {
+            System.out.println("Ordem crescente: " + valor2 + ", " + valor1);
+        }
+
+	}
+
+}
