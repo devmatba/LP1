3360b2e576af2b9e52211883bfc4011c9f2add0e
Matheus Barbosa <99769599+devmatba@users.noreply.github.com>
2024-08-22 14:01:30 -0300

    ✨ feat: Value Positivity
    Checks if a value is positive or negative, considering zero as positive.

---
+++ src/Ex8.java
@@ -0,0 +1,16 @@
+
+public class Ex8 {
+
+	public static void main(String[] args) {
+		
+		int valor = -5; 
+
+        if (valor >= 0) {
+            System.out.println("O valor é POSITIVO.");
+        } else {
+            System.out.println("O valor é NEGATIVO.");
+        }
+        
+	}
+
+}
