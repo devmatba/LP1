0fdcb3f9790413811e6e11c4be3b3911fed85387
Matheus Barbosa <99769599+devmatba@users.noreply.github.com>
2024-08-22 14:03:12 -0300

    ✨ feat: Voting Eligibility
    Checks if a person can vote this year based on the current year and their birth year.

---
+++ src/Ex11.java
@@ -0,0 +1,17 @@
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		
+		int anoNascimento = 2000;
+		int anoAtual = 2024;
+		
+		if(anoAtual - anoNascimento > 18) {
+			System.out.println("Pode votar.");
+		}else {
+			System.out.println("Não pode voltar.");
+		}
+
+	}
+
+}
