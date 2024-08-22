162f559d1d2a6aa0bfd51b98596ab4ad2b2c6e9f
Matheus Barbosa <99769599+devmatba@users.noreply.github.com>
2024-08-22 14:02:33 -0300

    ✨ feat: Average and School Approval
    Calculates the average of a student's grades and checks if they passed (grade equal to or greater than 6).

---
+++ src/Ex10.java
@@ -0,0 +1,17 @@
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		
+		double nota1a = 10;
+		double nota2a = 8;
+		
+		if ((nota1a + nota2a) / 2 >= 6 ){ 
+			System.out.println("Aprovado!");
+		}else {				
+			System.out.println("Reprovado!");
+			}
+			
+	  }
+
+}
