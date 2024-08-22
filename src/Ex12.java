601d5a74a9047182e91a37d1b4b610a45de8f894
Matheus Barbosa <99769599+devmatba@users.noreply.github.com>
2024-08-22 14:03:46 -0300

    ✨ feat: Greater Value
    Determines and displays the greater of two given values.

---
+++ src/Ex12.java
@@ -0,0 +1,17 @@
+
+public class Ex12 {
+
+	public static void main(String[] args) {
+		
+		int valor1 = 15;  
+        int valor2 = 22;  
+
+        if (valor1 > valor2) {
+            System.out.println("O maior valor é: " + valor1);
+        } else {
+            System.out.println("O maior valor é: " + valor2);
+        }
+
+	}
+
+}
